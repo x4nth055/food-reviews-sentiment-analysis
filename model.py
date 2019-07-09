1de2e0ebbc1d35eefe0c8f75dd1c74342e0921e8
x4nth055 <fullclip@protonmail.com>
2019-07-09 14:35:01 +0200

    Removed tensorflow/keras logs

--- model.py
+++ model.py
@@ -15,12 +15,13 @@ def get_model_binary(vocab_size, sequence_length):
     model.summary()
     return model
 
-def get_model_5stars(vocab_size, sequence_length, embedding_size):
+def get_model_5stars(vocab_size, sequence_length, embedding_size, verbose=0):
     model=Sequential()
     model.add(Embedding(vocab_size, embedding_size, input_length=sequence_length))
     model.add(SpatialDropout1D(0.15))
     model.add(LSTM(LSTM_units, recurrent_dropout=0.2))
     model.add(Dropout(0.3))
     model.add(Dense(1, activation="linear"))
-    model.summary()
+    if verbose:
+        model.summary()
     return model
--- test.py
+++ test.py
@@ -1,16 +1,28 @@
-# to use CPU
 import os
+# disable keras loggings
+import sys
+stderr = sys.stderr
+sys.stderr = open(os.devnull, 'w')
+import keras
+sys.stderr = stderr
+# to use CPU
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 
 config = tf.ConfigProto(intra_op_parallelism_threads=5,
+
                         inter_op_parallelism_threads=5, 
+
                         allow_soft_placement=True,
+
                         device_count = {'CPU' : 1,
+
                                         'GPU' : 0}
+
                        )
+
 from model import get_model_5stars
 from utils import clean_text, tokenize_words
 from config import embedding_size, sequence_length
@@ -18,10 +30,9 @@ from keras.preprocessing.sequence import pad_sequences
 
 import pickle
 
-print("Loading vocab2int")
 vocab2int = pickle.load(open("data/vocab2int.pickle", "rb"))
-
 model = get_model_5stars(len(vocab2int), sequence_length=sequence_length, embedding_size=embedding_size)
+
 model.load_weights("results/model_V20_0.38_0.80.h5")
 
 
@@ -29,15 +40,9 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Food Review evaluator")
     parser.add_argument("review", type=str, help="The review of the product in text")
-
     args = parser.parse_args()
 
     review = tokenize_words(clean_text(args.review), vocab2int)
     x = pad_sequences([review], maxlen=sequence_length)
+
     print(f"{model.predict(x)[0][0]:.2f}/5")
-
-    # test = "I think you should improve the products price thats really expensive but the product in general is not that good too"
-    # x = [ vocab2int[w.lower()] for w in test.split() ]
-
-    # x = pad_sequences([x], maxlen=sequence_length)
-    # print(model.predict(x))
